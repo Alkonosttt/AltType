3400f22ab698eb6a2060524725ebe50a6d5e53d1
Alkonosttt <voodoomon@protonmail.com>
2026-03-12 16:30:05 +0100

    Remove build folder from repo and add gitignore

--- build/debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_AltType_FILE /home/senorpomidor/Dev/AltType/build/debug/AltType)
-set(__QT_DEPLOY_TARGET_AltType_TYPE EXECUTABLE)
--- build/debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/senorpomidor/Dev/AltType
-# Build directory: /home/senorpomidor/Dev/AltType/build/debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
